0c20b55ee164449daa88ea335deba02b69a82b95
Dharaneish V C <dharaneishvc@gmail.com>
2022-01-11 20:46:55 +0530

    working

--- Book-store-Project/Main.py
+++ Book-store-Project/Main.py
@@ -34,30 +34,33 @@ class User:
         else:
             print("Wrong Password. Try again.")
 
+    @classmethod
     def checkusername(cls, arr, username):
         for i in arr:
             if(i.username == username):
                 return False
         return True   
 
+    @classmethod
     def retcount(cls):
         return User.Count
    
 
 class Seller(User):
     Count = 0
-    def __init__(self, username, password, name):
+    def __init__(self, username, password, publ):
         User.__init__(username, password)
-        self.name = name
+        self.publ = publ
         Seller.Count += 1
         self.books=[]
 
-    def getname(self):
-        return self.name
+    def getpubl(self):
+        return self.publ
 
-    def setname(self, name):
-        self.name = name
-            
+    def setpubl(self, publ):
+        self.publ = publ
+
+    @classmethod        
     def retcount(cls):
         return Seller.Count
 
@@ -66,25 +69,26 @@ class Seller(User):
 
     def diplay(self):
         print("UserName: " + self.__username)
-        print("Name: " + self.name)
+        print("Publication: " + self.publ)
         print("Mobile: " + self.mobile)
         print("Email: " + self.mail)
         print("My Books (IDs): " + self.books)
 
 class Customer(User):
     Count = 0
-    def __init__(self, username, password, publication):
+    def __init__(self, username, password, name):
         User.__init__(username, password)
-        self.publication = publication
+        self.name = name
         Seller.Count += 1
         self.cart=[]
 
-    def getpublication(self):
-        return self.publication
+    def getname(self):
+        return self.name
 
-    def setpublication(self, pub):
-        self.publication = pub
+    def setname(self, pub):
+        self.name = pub
 
+    @classmethod
     def retcount(cls):
         return Customer.Count
 
@@ -98,7 +102,7 @@ class Customer(User):
 
     def diplay(self):
         print("UserName: " + self.__username)
-        print("Publication: " + self.publication)
+        print("Name: " + self.name)
         print("Mobile: " + self.mobile)
         print("Email: " + self.mail)
         print("My Cart (PID, Count): " + self.cart)
@@ -113,11 +117,15 @@ class Books:
         self.seller = sid
         Books.Count += 1
 
+    @classmethod
     def checkpid(cls, arr, pid):
         for i in arr:
             if(i.pid == pid):
                 return False
         return True
+
+    def getid(self):
+        return self.pid
 
     def setname(self, name):
         self.name = name
@@ -159,10 +167,16 @@ class Books:
         self.seller = sell
 
     def purchase(self, count):
-        self.stock -= count
+        if(count <= self.stock):
+            self.stock -= count
+            return True
+        else:
+            return False
 
+    @classmethod
     def retcount(cls):
         return Books.Count
+
 
 class Paperbank(Books):
     Count = 0
@@ -179,6 +193,7 @@ class Paperbank(Books):
         self.weightgms = int(input("Enter Weight of Book(in gms):"))
         self.pages = int(input("Enter Total No of Pages(int): "))
 
+    @classmethod
     def retcount(cls):
         return Paperbank.Count
 
@@ -208,6 +223,7 @@ class Ebook(Books):
         self.filesize = float(input("Enter File Size of PDF(in megabytles):"))
         self.pages = int(input("Enter Total No of Pages(int): "))
 
+    @classmethod
     def retcount(cls):
         return Ebook.Count
 
@@ -223,13 +239,146 @@ class Ebook(Books):
         print("Total No of pagaes: "+ self.pages)
 
 
+
+def custlogin():
+    s = 0
+    uname = input("Enter Username:")
+    pwd = input("Enter Password")
+    for i in Customers:
+        if(i.login(uname,pwd)):
+            print("Log In Sucessfull")
+            s=1
+            break
+    if(s==0):
+        print("Log In Unsucessfull. Try gain. Return to main menu")
+    else:
+        c = 0
+        while(c != 5):
+            print("1. See Books \n2. Make Purchase \n3. View Details \n4. Edit Details and Reset Password \n5. Exit")
+            c = int(input())
+            while (i>5 or i<1):
+                c = int(input("Error! Enter No: "))
+            if(c==1):
+                print("Paperbanks:")
+                for q in Paperbanks:
+                    print("---------------------------------------");
+                    q.display()
+                print("--------------THE END-------------------");
+                print("Ebooks:")
+                for q in Ebooks:
+                    print("---------------------------------------");
+                    q.display()
+                print("--------------THE END-------------------");
+
+            elif(c==2):
+                id = int(input("Enter P ID: "))
+                cnt = int(input("Enter Count: "))
+                s=0
+                for q in Paperbanks:
+                    if q.getid == id :
+                        if(q.purchase(cnt)):
+                            i.purchase(id,cnt)
+                            print("Purchase made successful")
+                            s=1
+                            break
+                        else:
+                            print("Count you entered is more than Stock Available")
+                if s==0 :
+                    for q in Ebooks:
+                        if q.getid == id :
+                            if(q.purchase(cnt)):
+                                i.purchase(id,cnt)
+                                print("Purchase made successful")
+                                s=1
+                                break
+                            else:
+                                print("Count you entered is more than Stock Available")
+                if s==0:
+                    print("Product for the product ID is not Found")
+            elif(c==3):
+                i.display()
+            elif(c==4):
+                pass
+                #GET AND SET OF EACH FUNCTIONS, RESET PASSWORD
+            else:
+                print("Exiting to Main")
+                    
+
+
+def selllogin(): 
+    s = 0
+    uname = input("Enter Username:")
+    pwd = input("Enter Password")
+    for i in Sellers:
+        if(i.login(uname,pwd)):
+            print("Log In Sucessfull")
+            s=1
+            break
+    if(s==0):
+        print("Log In Unsucessfull. Try gain. Return to main menu")
+    else:
+        c = 0
+        while(c != 6):
+            print("1. See Books \n2. Add Books \n3. Edit Book Info \n4. View Details \n5. Edit Details and Reset Password \n6. Exit")
+            c = int(input())
+            while (i>6 or i<1):
+                c = int(input("Error! Enter No: "))
+            if(c==1):
+                print("Paperbanks:")
+                for q in Paperbanks:
+                    print("---------------------------------------");
+                    q.display()
+                print("--------------THE END-------------------");
+                print("Ebooks:")
+                for q in Ebooks:
+                    print("---------------------------------------");
+                    q.display()
+                print("--------------THE END-------------------");
+
+            elif(c==2):
+                id = int(input("Enter P ID: "))
+                if(Books.checkpid(Paperbanks, id) and Books.checkpid(Ebooks,id)):
+                    c = int(input("1 PaperBank \n 2 Ebook \n 3 Exit \n Enter: "))
+                    while(c!=1 or c!=2 or c!=3):
+                        c=int(input("Error. Enter again(1-3): "))
+                    if(c==1):
+                        stock = int(input("Enter stock size: "))
+                        temp = Paperbank(id,stock,i.getid())
+                        temp.setvalues
+                        Paperbank.append(temp)
+                        print("Book added succesfully")
+                    elif(c==2):
+                        stock = int(input("Enter stock size: "))
+                        temp = Ebooks(id,stock,i.getid())
+                        temp.setvalues
+                        Paperbank.append(temp)
+                        print("Book added succesfully")
+                    else:
+                        print("Book Not added!")
+                else:
+                    print("ID is unavailable. Try Again")
+
+            elif(c==3):
+                pass
+            #GET AND SET FUNCTIONS IN BOOK
+        
+                
+                if s==0:
+                    print("Product for the product ID is not Found")
+            elif(c==4):
+                i.display()
+            elif(c==5):
+                pass
+                #GET AND SET OF EACH FUNCTIONS, RESET PASSWORD
+            else:
+                print("Exiting to Main")
+
 print("WELCOME TO BOOK STORE")
 c = 0
 Sellers = []
 Customers = []
 Paperbanks = []
 Ebooks = []
-
 
 while(c != 5):
     print("1. Customer Log In \n 2. Seller Log In \n 3. Customer Sign Up \n 4. Seller Sign Up \n 5.Exit \nEnter:")
@@ -242,43 +391,30 @@ while(c != 5):
         selllogin()
 
     elif c==3:
-        
+        uid = input("Enter a unique User ID")
+        if(User.checkusername(Sellers, uid) and User.checkusername(Customers, uid)):
+            pass1 = input("Enter Password: ")
+            if(pass1==input("Re-enter Password:")):
+                name = input("Enter Name")
+                temp = Customer(uid, pass1, name)
+                Customers.append(temp);
+                print("User ID created. Log in and add your mobile, email")
+        else:
+            print("Username unavailable")
+         
 
     elif c==4:
+        uid = input("Enter a unique User ID")
+        if(User.checkusername(Sellers, uid) and User.checkusername(Customers, uid)):
+            pass1 = input("Enter Password: ")
+            if(pass1==input("Re-enter Password:")):
+                publ = input("Enter Publication Name:")
+                temp = Seller(uid, pass1, publ)
+                Sellers.append(temp);
+                print("User ID created. Log in and add your mobile, email")
+        else:
+            print("Username unavailable")
 
     else:
         print("Thank You for using this product")
 
-
-def custlogin():
-    s = 0
-        uname = input("Enter Username:")
-        pwd = input("Enter Password")
-        for i in Customers:
-            if(i.login(uname,pwd)):
-                print("Log In Sucessfull")
-                s=1
-                break
-        if(s==0):
-            print("Log In Unsucessfull")
-        else:
-            c = 0
-            while(c != 5):
-                print("1. See Books \n2. Make Purchase \n3. View Details \n4. Edit Details and Reset Password \n5. Exit")
-                c = int(input())
-                if(c==1):
-                    
-
-
-def selllogin(): 
-    s = 0
-        uname = input("Enter Username:")
-        pwd = input("Enter Password")
-        for i in Sellers:
-            if(i.login(uname,pwd)):
-                print("Log In Sucessfull")
-                s=1
-                break
-        if(s==0):
-            print("Log In Unsucessfull")
-        else: 
